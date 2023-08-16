16ce0486ea8d03482edd9d29c31a8c0d38d6754f
cpshabariram <113747502+cpshabariram@users.noreply.github.com>
2023-08-16 15:26:43 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, render_template
+import requests
+
+app = Flask(__name__,template_folder='Templates')
+
+@app.route('/', methods =["GET", "POST"])
+def index():
+    weatherData = ''
+    error = 0
+    cityName = ''
+    if request.method == "POST":
+        cityName = request.form.get("cityName")
+        if cityName:
+            weatherApiKey = 'ca6c4e3ef5e4e9ca0f93d129ed363223'
+            url = "https://api.openweathermap.org/data/2.5/weather?q="+cityName+"&appid=" + we>
+            weatherData = requests.get(url).json()
+        else:
+            error = 1
+    return render_template('index.html', data = weatherData, cityName = cityName, error = erro>
+if __name__ == "__main__":
+        app.debug = True
+        app.run()
+
